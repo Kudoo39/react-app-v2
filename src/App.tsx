c9f2ac10f410aac272c253755208774142d6b4ce
Kudoo39 <thpthv.nguyenphuduykhang@gmail.com>
2023-07-22 15:41:02 +0300

    updating objects

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,29 @@
-import Message from "./components/Message";
+import { useState } from "react";
 
 const App = () => {
+  const [drink, setDrink] = useState({
+    title: "Americano",
+    price: 5,
+  });
+
+  //UPDATING OBJECTS
+  // const handleClick = () => {
+  //   const newDrink = {
+  //     title: drink.title,
+  //     price: 6,
+  //   };
+  //   setDrink(newDrink);
+  // };
+
+  //GOOD WAY TO UPDATE
+  const handleClick = () => {
+    setDrink({ ...drink, price: 6 });
+  };
+
   return (
     <div>
-      <Message />
-      <Message />
-      <Message />
+      {drink.price}
+      <button onClick={handleClick}>Click Me</button>
     </div>
   );
 };
