c045f40f7866948353ed605f864065a392e55511
Kudoo39 <thpthv.nguyenphuduykhang@gmail.com>
2023-07-29 00:03:05 +0300

    update state complicated nested object and array

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,30 @@
 import { useState } from "react";
 const App = () => {
-  const [pizza, setPizza] = useState({
-    name: "Spicy Pepperoni",
-    toppings: ["Mushroom"],
+  const [cart, setCart] = useState({
+    discount: 1,
+    items: [
+      { id: 1, title: "Product 1", quantity: 1 },
+      { id: 2, title: "Product 2", quantity: 1 },
+    ],
   });
 
   const handleClick = () => {
-    setPizza({ ...pizza, toppings: [...pizza.toppings, ", Cheese"] });
+    setCart({
+      ...cart,
+      items: cart.items.map((item) =>
+        item.id === 1 ? { ...item, quantity: 2 } : item
+      ),
+    });
   };
 
   return (
     <div>
-      {pizza.toppings}
-      <button onClick={handleClick}>Adding toppings</button>
+      {cart.items.map((item) => (
+        <div key={item.id}>
+          {item.title} - Quantity: {item.quantity}
+        </div>
+      ))}
+      <button onClick={handleClick}>Updating items</button>
     </div>
   );
 };
