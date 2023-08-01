5e8460ee08d8e6995019adb1fcb8f82f936dfc33
Kudoo39 <thpthv.nguyenphuduykhang@gmail.com>
2023-08-01 17:10:15 +0300

    First step building a Form

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,8 @@
 import React from "react";
-import ExpandableText from "./components/ExpandableText";
+import Form from "./components/Form";
 
 const App = () => {
-  const longContent =
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vitae metus id tellus rhoncus tempus. Donec quis orci a massa tristique auctor et a ex. Duis hendrerit odio nec tellus rhoncus, eu tempor arcu tristique. Vestibulum nec quam turpis. Sed vel mi sed nisl fermentum gravida. Nunc tincidunt lectus nec neque faucibus, ut pharetra eros cursus. Duis eu consequat justo, eget consectetur metus.";
-
-  return (
-    <>
-      <div>
-        <ExpandableText>{longContent}</ExpandableText>
-      </div>
-    </>
-  );
+  return <Form />;
 };
 
 export default App;
