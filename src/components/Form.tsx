0bdf4fbcbea3b6ce4c81a6abce4b5ab712a1276b
Kudoo39 <thpthv.nguyenphuduykhang@gmail.com>
2023-08-02 22:20:38 +0300

    Schema based Validation with Zod

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,13 +1,20 @@
-import React, { FormEvent, useState } from "react";
 import { useForm, FieldValues } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
 
-interface FormData {
-  name: string;
-  age: number;
-}
+const schema = z.object({
+  name: z.string().min(2, { message: "Name must be longer than 2 characters" }),
+  age: z
+    .number({ invalid_type_error: "Age field is required" })
+    .min(18, { message: "Age must be over 18!" }),
+});
+
+type FormData = z.infer<typeof schema>;
 
 const Form = () => {
-  const { register, handleSubmit, formState } = useForm<FormData>();
+  const { register, handleSubmit, formState } = useForm<FormData>({
+    resolver: zodResolver(schema),
+  });
 
   const onSubmit = (data: FieldValues) => console.log(data);
 
@@ -18,16 +25,13 @@ const Form = () => {
           Name
         </label>
         <input
-          {...register("name", { required: true, minLength: 2 })}
+          {...register("name")}
           id="name"
           type="text"
           className="form-control"
         />
-        {formState.errors.name?.type === "required" && (
-          <p className="text-danger">The name field is required!</p>
-        )}
-        {formState.errors.name?.type === "minLength" && (
-          <p className="text-danger">The name must be at least 2 characters!</p>
+        {formState.errors.name && (
+          <p className="text-danger">{formState.errors.name.message}</p>
         )}
       </div>
       <div className="mb-3">
@@ -35,11 +39,14 @@ const Form = () => {
           Age
         </label>
         <input
-          {...register("age")}
+          {...register("age", { valueAsNumber: true })}
           id="age"
           type="number"
           className="form-control"
         />
+        {formState.errors.age && (
+          <p className="text-danger">{formState.errors.age.message}</p>
+        )}
       </div>
       <button className="btn btn-primary" type="submit">
         Submit
