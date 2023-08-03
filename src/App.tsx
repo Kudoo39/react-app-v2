80cf47b8b863a72d6edced145c8db4c7435ef562
Kudoo39 <thpthv.nguyenphuduykhang@gmail.com>
2023-08-03 22:43:12 +0300

    solution for building ExpenseList

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,33 @@
-import React from "react";
-import ExpenseList from "./expense-tracker/ExpenseList";
+import React, { useState } from "react";
+import ExpenseList from "./expense-tracker/components/ExpenseList";
 
 const App = () => {
-  return <ExpenseList />;
+  const [expenses, setExpenses] = useState([
+    {
+      id: 1,
+      description: "Milk",
+      amount: 10,
+      category: "Groceries",
+    },
+    {
+      id: 2,
+      description: "Egg",
+      amount: 10,
+      category: "Groceries",
+    },
+    {
+      id: 3,
+      description: "Butter",
+      amount: 10,
+      category: "Groceries",
+    },
+  ]);
+  return (
+    <ExpenseList
+      expenses={expenses}
+      onDelete={(id) => setExpenses(expenses.filter((e) => e.id !== id))}
+    />
+  );
 };
 
 export default App;
